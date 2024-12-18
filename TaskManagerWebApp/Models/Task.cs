8e9b861c8ab33c1b945b83b9c25ac301613c9a0f
Isaac Chavarría <89037579+Txaverria@users.noreply.github.com>
2024-12-18 13:18:18 -0600

    Create Task Model

--- TaskManagerWebApp/Models/Task.cs
+++ TaskManagerWebApp/Models/Task.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace TaskManagerWebApp.Models
+﻿namespace TaskManagerWebApp.Models
 {
     public class Task
     {
-        
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+        public DateTime? DueDate { get; set; }
     }
 }
